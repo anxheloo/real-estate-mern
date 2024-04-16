03912a1f7d05f97fbb942aaadc4b8ec3f40e4c9f
Anxheloo <anxhelocenollari@gmail.com>
2024-04-16 12:21:56 +0200

    creating auth route, controller, json() middleware and hashing password using bcryptjs

--- real-estate-backend/index.js
+++ real-estate-backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
 
 //setting up dotenv
 import dotenv from "dotenv";
@@ -16,8 +17,13 @@ mongoose
 //We install nodemon and set the script to use nodemon index.js => "dev": "nodemon index.js",  "start": "node index.js" => for deployment
 const app = express();
 
+//middleware
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+//routes
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
