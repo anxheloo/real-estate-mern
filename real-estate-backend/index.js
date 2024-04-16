1542db0388c8eb1dd63c7ab01f1287f3f1366d59
Anxheloo <anxhelocenollari@gmail.com>
2024-04-16 11:23:25 +0200

    connecting to db

--- real-estate-backend/index.js
+++ real-estate-backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO).then();
+//setting up dotenv
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDb");
+  })
+  .catch((error) => console.log(error));
 
 //We install nodemon and set the script to use nodemon index.js => "dev": "nodemon index.js",  "start": "node index.js" => for deployment
 const app = express();
