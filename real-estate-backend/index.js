63eed98d820c1d9809601c04853ae828d9c51cac
Anxheloo <anxhelocenollari@gmail.com>
2024-04-16 11:19:03 +0200

    adding gitignore in the root

--- real-estate-backend/index.js
+++ real-estate-backend/index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO).then();
 
 //We install nodemon and set the script to use nodemon index.js => "dev": "nodemon index.js",  "start": "node index.js" => for deployment
 const app = express();
