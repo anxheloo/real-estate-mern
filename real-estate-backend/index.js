345c7308fbf94049a23f7731a86f663f91c81bfa
Anxheloo <anxhelocenollari@gmail.com>
2024-04-19 10:36:11 +0200

    create listing api route

--- real-estate-backend/index.js
+++ real-estate-backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 //setting up dotenv
@@ -29,6 +30,7 @@ app.listen(3000, () => {
 //routes
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 // global error-handling middleware
 app.use((error, req, res, next) => {
