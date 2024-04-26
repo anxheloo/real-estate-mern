158a07fa5ba2f6753004e980bcc09f195fc119c0
Anxheloo <anxhelocenollari@gmail.com>
2024-04-26 17:36:39 +0200

    making ready for render

--- real-estate-backend/index.js
+++ real-estate-backend/index.js
@@ -4,6 +4,7 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 //setting up dotenv
 import dotenv from "dotenv";
@@ -15,6 +16,8 @@ mongoose
     console.log("Connected to MongoDb");
   })
   .catch((error) => console.log(error));
+
+const __dirname = path.resolve();
 
 //We install nodemon and set the script to use nodemon index.js => "dev": "nodemon index.js",  "start": "node index.js" => for deployment
 const app = express();
@@ -31,6 +34,13 @@ app.listen(3000, () => {
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing", listingRouter);
+
+app.use(express.static(path.join(__dirname, "/real-estate-frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "real-estate-frontend", "dist", "index.html")
+  );
+});
 
 // global error-handling middleware
 app.use((error, req, res, next) => {
